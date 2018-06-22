83a277b3a570f009534749530812bf579695a157
Selvin11 <528370630@qq.com>
2018-06-22 18:48:53 +0800

    feat: 完成col、row组件

---
+++ packages/col/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view :class="['i-col', span ? 'i-col-span-' + span : '', offset ? 'i-col-offset-' + offset : '']">
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'iCol',
+  props: {
+    iClass: String,
+    span: {
+      default: 0,
+      type: Number
+    },
+    offset: {
+      default: 0,
+      type: Number
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import './index.less';
+</style>
+
---
+++ packages/row/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="i-row" :class="iClass">
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'iRow',
+  props: {
+    iClass: String
+  }
+}
+</script>
+
+<style lang="less">
+@import './index.less';
+</style>
+
