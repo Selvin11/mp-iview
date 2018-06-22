66823c28ab9c5e4ac4b292eb502b118740a8a52d
Selvin11 <528370630@qq.com>
2018-06-22 18:49:34 +0800

    feat: 完成drawer组件

---
+++ packages/drawer/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="['i-drawer', visible ? 'i-drawer-show' : '', 'i-drawer-' + mode, iClass]">
+    <div v-if="mask" class="i-drawer-mask" @click="handleMaskClick"></div>
+    <div class="i-drawer-container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iDrawer',
+  props: {
+    iClass: String,
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    mask: {
+      type: Boolean,
+      default: true
+    },
+    maskClosable: {
+      type: Boolean,
+      default: true
+    },
+    mode: {
+      type: String,
+      default: 'left' // left right
+    }
+  },
+  methods: {
+    handleMaskClick () {
+      if (!this.maskClosable) return
+      this.$emit('close', {})
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import './index.less';
+</style>
---
+++ src/pages/drawer/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="page">
+    <div class="page__hd">
+      <div class="page__title">Drawer</div>
+      <div class="page__desc">抽屉</div>
+    </div>
+    <div class="page__bd">
+      <i-button @click="toggleLeft1" type="ghost">左边弹出1</i-button>
+      <i-button @click="toggleLeft2" type="primary">左边弹出2</i-button>
+      <i-button @click="toggleRight1" type="ghost">右边弹出1</i-button>
+      <i-button @click="toggleRight2" type="primary">右边弹出2</i-button>
+      <i-drawer mode="left" :visible="showLeft1" @close="toggleLeft1">
+        <view class="demo-container">
+            单击遮罩层关闭
+        </view>
+      </i-drawer>
+      <i-drawer mode="left" :visible="showLeft2" :mask-closable="false">
+        <view class="demo-container">
+            禁止单击遮罩关闭
+            <i-button @click="toggleLeft2" type="primary">关闭</i-button>
+        </view>
+      </i-drawer>
+      <i-drawer mode="right" :visible="showRight1" @close="toggleRight1">
+        <view class="demo-container">
+            单击遮罩层关闭
+        </view>
+      </i-drawer>
+      <i-drawer mode="right" :visible="showRight2" :mask-closable="false">
+        <view class="demo-container">
+            禁止单击遮罩关闭
+            <i-button @click="toggleRight2" type="primary">关闭</i-button>
+        </view>
+      </i-drawer>
+    </div>
+  </div>
+</template>
+
+<script>
+import iDrawer from '../../../packages/drawer'
+import iButton from '../../../packages/button'
+
+export default {
+  data () {
+    return {
+      showLeft1: false,
+      showLeft2: false,
+      showRight1: false,
+      showRight2: false
+    }
+  },
+  components: {
+    iDrawer,
+    iButton
+  },
+  methods: {
+    toggleLeft1 () {
+      this.showLeft1 = !this.showLeft1
+    },
+    toggleLeft2 () {
+      this.showLeft2 = !this.showLeft2
+    },
+    toggleRight1 () {
+      this.showRight1 = !this.showRight1
+    },
+    toggleRight2 () {
+      this.showRight2 = !this.showRight2
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.demo-container{
+	width:50vw;
+	height: 100vh;
+	background:#fff;
+}
+</style>
