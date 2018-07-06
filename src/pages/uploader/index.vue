5dade7f96cae880b86f66a65c0275705165b5c23
Rick Tang <rick.tang@cheanjia.com>
2018-07-06 16:47:05 +0800

    更新uploader组件样式

--- src/pages/uploader/index.vue
+++ src/pages/uploader/index.vue
@@ -2,16 +2,21 @@
   <div class="page">
     <div class="page__hd">
       <div class="page__title">Uploader</div>
-      <div class="page__desc">上传组件</div>
+      <div class="page__desc">文件上传</div>
     </div>
     <div class="page__bd">
-      <Uploader />
+      <Uploader
+        @onSelect="onSelect"
+        :file-list="files"
+        title="文件上传"
+        :max="max" />
     </div>
   </div>
 </template>
 
 <script>
-import Uploader from '../../../packages/uploader'
+import Vue from 'vue'
+import Uploader from '../../../packages/uploader-base'
 
 export default {
   data () {
@@ -22,6 +27,21 @@ export default {
   },
   components: {
     Uploader
+  },
+  methods: {
+    onSelect () {
+      wx.chooseImage({
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        count: this.max,
+        success: (res) => {
+          this.files = this.files.concat(res.tempFiles)
+          if (this.files[0]) {
+            Vue.set(this.files[0], 'failure', true)
+          }
+        }
+      })
+    }
   }
 }
 </script>
