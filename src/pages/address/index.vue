a5f78d83b2d35c7ffaa8603e39fb7e3f80040eca
Selvin11 <528370630@qq.com>
2018-09-18 17:13:23 +0800

    feat: 新增地址选择组件

---
+++ src/pages/address/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <i-address
+      title="车辆归属地"
+      @on-change="addressChange" 
+      :address-data="addressData"
+    >
+    </i-address>
+  </div>
+</template>
+
+<script>
+import iAddress from '../../../packages/address'
+export default {
+  components: {
+    iAddress
+  },
+  data () {
+    return {
+      addressData: {
+        province: {
+          code: '130000',
+          name: '河北省'
+        },
+        city: {
+          code: '130100',
+          name: '石家庄市'
+        },
+        district: {
+          code: '130102',
+          name: '长安区'
+        }
+      }
+    }
+  },
+  methods: {
+    addressChange (address) {
+      console.log(address)
+    }
+  }
+}
+</script>
